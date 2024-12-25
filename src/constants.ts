388f2f6cf6b751898161d9266fee4cef479a2b6d
Slava <slavathedeveloper@gmail.com>
2024-12-26 01:38:07 +0300

    fix TG_BG_NAME

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const LIBRARY_BASE_URL = 'https://vedabase.io/ru/library/';
 export const TG_BASE_URL = 'https://t.me/';
-export const TG_BG_NAME = 'bgverses';
+export const TG_BG_NAME = 'bg_verses';
 export const TG_BG_FIRST_VERSE_ID = 2;
 
 export const DAILY_VERSE_KEY = 'dailyVerse';
